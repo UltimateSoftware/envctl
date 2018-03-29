a3277ec8b7f6b943bafc9eeb177a65135b157690
Hugo Torres <hugo_torres@ultimatesoftware.com>
2018-03-29 10:10:38 -0400

    Add tests for getBuildContext()
    With this, the low hanging fruit of the Docker package is done. From now
    on any test additions will probably require some refactoring.

--- pkg/docker/image_test.go
+++ pkg/docker/image_test.go
@@ -1,7 +1,9 @@
 package docker
 
 import (
+	"archive/tar"
 	"fmt"
+	"io"
 	"strings"
 	"testing"
 )
@@ -26,6 +28,43 @@ func TestBuildDockerfile(t *testing.T) {
 	actual := buf.String()
 	if expected != actual {
 		prettyFail(t, "Dockerfile build", expected, actual)
+	}
+}
+
+func TestGetBuildContext(t *testing.T) {
+	testcfg := ImageConfig{
+		BaseImage: "scratch",
+		Mount:     "/test-path",
+		Shell:     "/testsh",
+	}
+
+	buf, err := buildDockerfile(testcfg)
+	if err != nil {
+		prettyFail(t, "Dockerfile build", "no errors", err)
+	}
+
+	bldctx, err := getBuildContext(buf)
+	if err != nil {
+		prettyFail(t, "getBuildContext()", "no errors", err)
+	}
+
+	tarrd := tar.NewReader(bldctx)
+	h, err := tarrd.Next()
+	for err != io.EOF {
+		// The only thing in here should be the Dockerfile. If this is the case,
+		// the for loop will exit before hitting this check again. If it's not,
+		// it will fail because whatever is next won't match this.
+		if h.FileInfo().Name() != "Dockerfile" {
+			prettyFail(t, "tar header", "Dockerfile", h.FileInfo().Name())
+		}
+
+		expected := int64(buf.Len())
+		actual := h.FileInfo().Size()
+		if expected != actual {
+			prettyFail(t, "tar file size", expected, actual)
+		}
+
+		h, err = tarrd.Next()
 	}
 }
 
