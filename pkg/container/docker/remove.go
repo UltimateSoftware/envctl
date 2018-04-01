5bc4216e3ca768597cedc01e71834aec973ed978
Hugo Torres <hugo_torres@ultimatesoftware.com>
2018-04-01 13:57:30 -0400

    Refactor new docker remove to satisfy interface
    The `*Controller` type needs to satisfy the `Controller` interface. This
    includes handling image removal in the same function as the container
    removal.

--- pkg/container/docker/remove.go
+++ pkg/container/docker/remove.go
@@ -5,10 +5,16 @@ import (
 
 	"github.com/UltimateSoftware/envctl/pkg/container"
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
 )
 
-// RemoveContainer removes the container with the given `id`.
-func (c *Controller) RemoveContainer(m container.Metadata) error {
+// Remove removes the container with the given metadata.
+func (c *Controller) Remove(m container.Metadata) error {
+	err := c.removeImage(m.ImageID)
+	if err != nil {
+		return err
+	}
+
 	return c.client.ContainerRemove(
 		context.Background(),
 		m.ID,
@@ -17,4 +23,32 @@ func (c *Controller) RemoveContainer(m container.Metadata) error {
 			Force:         true,
 		},
 	)
+}
+
+func (c *Controller) removeImage(name string) error {
+	args := filters.NewArgs()
+	args.Add("reference", name)
+
+	rmopts := types.ImageRemoveOptions{
+		PruneChildren: true,
+		Force:         true,
+	}
+
+	lsopts := types.ImageListOptions{
+		Filters: args,
+	}
+
+	imgs, err := c.client.ImageList(context.Background(), lsopts)
+	if err != nil {
+		return err
+	}
+
+	for _, img := range imgs {
+		_, err := c.client.ImageRemove(context.Background(), img.ID, rmopts)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
