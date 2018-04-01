e7e4c1497a6f018e6b37bf30e85547abdcf29340
Hugo Torres <hugo_torres@ultimatesoftware.com>
2018-04-01 13:48:52 -0400

    Add container creation to new container package

--- pkg/container/container.go
+++ pkg/container/container.go
@@ -1,16 +1,25 @@
 package container
 
+import "fmt"
+
 // Metadata is what's returned by the container functions. It contains
 // everything that a consumer of this package needs to know about containers
 // being managed.
 type Metadata struct {
 	ID        string
+	ImageID   string
 	BaseName  string
 	BaseImage string
 	Shell     string
-	Mount     string
+	Mounts    []Mount
 	WorkDir   string
 	Envs      []string
+}
+
+// Mount is directory on the host paired with a volume mount point.
+type Mount struct {
+	Source      string
+	Destination string
 }
 
 // Controller can control containers. This includes allowing consumers to
@@ -20,4 +29,8 @@ type Controller interface {
 	Remove(Metadata) error
 	Attach(Metadata) error
 	Run(Metadata, []string) error
+}
+
+func (b Mount) String() string {
+	return fmt.Sprintf("%v:%v", b.Source, b.Destination)
 }
---
+++ pkg/container/docker/create.go
@@ -0,0 +1,139 @@
+package docker
+
+import (
+	"archive/tar"
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+
+	"github.com/UltimateSoftware/envctl/pkg/container"
+	"github.com/alecthomas/template"
+	"github.com/docker/docker/api/types"
+	docker "github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
+	"github.com/google/uuid"
+)
+
+func (c *Controller) Create(m container.Metadata) (container.Metadata, error) {
+	img, err := c.buildImage(m)
+	if err != nil {
+		return container.Metadata{}, err
+	}
+
+	m.ImageID = img
+
+	ccfg := &docker.Config{
+		User:      "root",
+		Tty:       true,
+		Image:     m.ImageID,
+		OpenStdin: true,
+		Env:       m.Envs,
+	}
+
+	hcfg := &docker.HostConfig{
+		Binds: make([]string, len(m.Mounts)),
+	}
+
+	ncfg := &network.NetworkingConfig{}
+
+	for i, mount := range m.Mounts {
+		hcfg.Binds[i] = mount.String()
+	}
+
+	cnt, err := c.client.ContainerCreate(
+		context.Background(),
+		ccfg,
+		hcfg,
+		ncfg,
+		m.BaseName,
+	)
+	if err != nil {
+		return container.Metadata{}, err
+	}
+
+	m.ID = cnt.ID
+	return m, nil
+}
+
+var dockerfileTpl = `FROM {{ .BaseImage }}
+	VOLUME ["{{ .Mount }}"]
+	WORKDIR "{{ .Mount }}"
+	ENTRYPOINT ["{{ .Shell }}"]`
+
+// buildImage will build an image based on the passed in ImageConfig. It returns
+// the name of the built image, as <cfg.BaseName:UUID>, or an error.
+//
+// buildImage blocks until the image build has finished and the API is done
+// streaming the output back.
+func (c *Controller) buildImage(m container.Metadata) (string, error) {
+	dockerfile, err := buildDockerfile(m)
+	if err != nil {
+		return "", err
+	}
+
+	buildContext, err := getBuildContext(dockerfile)
+	if err != nil {
+		return "", err
+	}
+
+	name := fmt.Sprintf("%v:%v", m.BaseName, uuid.New().String())
+	bldopts := types.ImageBuildOptions{
+		Tags: []string{name},
+	}
+
+	resp, err := c.client.ImageBuild(context.Background(), buildContext, bldopts)
+	if err != nil {
+		return "", err
+	}
+
+	// the read MUST happen, if not the program will continue without waiting
+	// for the build to complete
+	io.Copy(ioutil.Discard, resp.Body)
+	defer resp.Body.Close()
+
+	return name, nil
+}
+
+func buildDockerfile(m container.Metadata) (*bytes.Buffer, error) {
+	buf := &bytes.Buffer{}
+
+	tpl, err := template.New("Dockerfile").Parse(dockerfileTpl)
+	if err != nil {
+		return &bytes.Buffer{}, err
+	}
+
+	err = tpl.Execute(buf, &m)
+	if err != nil {
+		return &bytes.Buffer{}, err
+	}
+
+	return buf, nil
+}
+
+func getBuildContext(raw *bytes.Buffer) (*bytes.Buffer, error) {
+	buf := bytes.NewBuffer([]byte{})
+	wr := tar.NewWriter(buf)
+
+	hdr := &tar.Header{
+		Name: "Dockerfile",
+		Mode: 0600,
+		Size: int64(raw.Len()),
+	}
+
+	if err := wr.WriteHeader(hdr); err != nil {
+		return &bytes.Buffer{}, err
+	}
+
+	if _, err := wr.Write(raw.Bytes()); err != nil {
+		return &bytes.Buffer{}, err
+	}
+
+	padlen := 512 - (buf.Len() % 512)
+	padding := make([]byte, padlen)
+
+	padded := bytes.NewBuffer(append(buf.Bytes(), padding...))
+
+	return padded, nil
+}
