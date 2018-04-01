87a5a7b7e1c9cf5a245b960d123207779e0e4fc2
Hugo Torres <hugo_torres@ultimatesoftware.com>
2018-04-01 16:59:34 -0400

    Refactor init to use new generator pattern
    `init` doesn't benefit from the new dependency injection, but for the
    sake of anybody else reading the code it should follow the same pattern
    as all the other commands.

--- cmd/init.go
+++ cmd/init.go
@@ -1,62 +1,67 @@
 package cmd
 
-// var initDesc = "Initialize development environment"
+import (
+	"fmt"
+	"os"
 
-// var initLongDesc = `init - Initialize development environment
+	"github.com/UltimateSoftware/envctl/internal/print"
+	"github.com/spf13/cobra"
+)
 
-// "init" will generate a file called "envctl.yml" (or whatever was passed into
-// --config) in the current directory.
+func newInitCmd() *cobra.Command {
+	initDesc := "Initialize development environment"
 
-// This file has sane defaults, but might need to be edited, and should be checked
-// into version control.
-// `
+	initLongDesc := `init - Initialize development environment
 
-// // initCmd represents the init command
-// var initCmd = &cobra.Command{
-// 	Use:   "init",
-// 	Short: initDesc,
-// 	Long:  initLongDesc,
-// 	Run:   runInit,
-// }
+"init" will generate a file called "envctl.yml" (or whatever was passed into
+--config) in the current directory.
 
-// func init() {
-// 	rootCmd.AddCommand(initCmd)
-// }
+This file has sane defaults, but might need to be edited, and should be checked
+into version control.
+`
 
-// func runInit(cmd *cobra.Command, args []string) {
-// 	tpl := `---
-// image: ubuntu:latest
+	tpl := `---
+image: ubuntu:latest
 
-// shell: /bin/bash
+shell: /bin/bash
 
-// bootstrap:
-// - echo 'Environment initialized' > /envctl
+bootstrap:
+- echo 'Environment initialized' > /envctl
 
-// variables:
-// - FOO=bar
-// `
+variables:
+- FOO=bar
+`
 
-// 	fmt.Print("creating config file... ")
+	runInit := func(cmd *cobra.Command, args []string) {
+		fmt.Print("creating config file... ")
 
-// 	if _, err := os.Stat(cfgFile); err == nil {
-// 		print.Error()
-// 		fmt.Printf("cannot overwrite %v\n", cfgFile)
-// 		os.Exit(1)
-// 	}
+		if _, err := os.Stat(cfgFile); err == nil {
+			print.Error()
+			fmt.Printf("cannot overwrite %v\n", cfgFile)
+			os.Exit(1)
+		}
 
-// 	f, err := os.OpenFile(cfgFile, os.O_WRONLY|os.O_CREATE, 0644)
-// 	if err != nil {
-// 		print.Error()
-// 		fmt.Printf("error opening %v: %v\n", cfgFile, err)
-// 		os.Exit(1)
-// 	}
+		f, err := os.OpenFile(cfgFile, os.O_WRONLY|os.O_CREATE, 0644)
+		if err != nil {
+			print.Error()
+			fmt.Printf("error opening %v: %v\n", cfgFile, err)
+			os.Exit(1)
+		}
 
-// 	_, err = f.WriteString(tpl)
-// 	if err != nil {
-// 		print.Error()
-// 		fmt.Printf("error writing %v: %v\n", cfgFile, err)
-// 		os.Exit(1)
-// 	}
+		_, err = f.WriteString(tpl)
+		if err != nil {
+			print.Error()
+			fmt.Printf("error writing %v: %v\n", cfgFile, err)
+			os.Exit(1)
+		}
 
-// 	print.OK()
-// }
+		print.OK()
+	}
+
+	return &cobra.Command{
+		Use:   "init",
+		Short: initDesc,
+		Long:  initLongDesc,
+		Run:   runInit,
+	}
+}
--- cmd/root.go
+++ cmd/root.go
@@ -56,6 +56,7 @@ func init() {
 	rootCmd.AddCommand(newCreateCmd(ctl, s))
 	rootCmd.AddCommand(newDestroyCmd(ctl, s))
 	rootCmd.AddCommand(newStatusCmd(s))
+	rootCmd.AddCommand(newInitCmd())
 
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
