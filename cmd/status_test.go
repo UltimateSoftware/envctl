5229eec5736c1d678da4e3ece8c8bd85b43fae14
Hugo Torres <hugo_torres@ultimatesoftware.com>
2018-04-02 18:07:58 -0400

    Add test for status command in error state
    Part of #3

--- cmd/status_test.go
+++ cmd/status_test.go
@@ -84,3 +84,32 @@ Run "envctl login" to enter it.
 		}
 	}
 }
+
+func TestErrorStatus(got *testing.T) {
+	t := test_pkg.NewT(got)
+	s := &memStore{
+		env: db.Environment{
+			Status: db.StatusError,
+		},
+	}
+
+	cmd := newStatusCmd(s)
+
+	outch, errch := test_pkg.HijackStdout(func() {
+		cmd.Run(cmd, []string{})
+	})
+
+	expected := `Something is wrong with the environment. :(
+
+Try recreating it by running "envctl destroy", followed by "envctl create".
+`
+
+	select {
+	case err := <-errch:
+		t.Fatal("hijacking output", nil, err)
+	case actual := <-outch:
+		if expected != string(actual) {
+			t.Fatal("output", expected, string(actual))
+		}
+	}
+}
