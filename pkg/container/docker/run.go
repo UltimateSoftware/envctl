1ece64cc2a69b7deafd1394acc96711cbddc2b65
Hugo Torres <hugo_torres@ultimatesoftware.com>
2018-04-18 23:57:29 -0400

    Show output of bootstrap on create
    This is needed because users should be able to see what's going on as
    their bootstrap steps are running. By definition, this also fixes the
    bug where create returns before the bootstrap steps have actually
    finished running.
    
    This is done in much the same way that `login` attaches to the
    container, but the terminal doesn't need to be put in raw mode and only
    the hijacked reader is copied to stdout.
    
    Resolves #11 and #10.

--- pkg/container/docker/run.go
+++ pkg/container/docker/run.go
@@ -2,37 +2,80 @@ package docker
 
 import (
 	"context"
+	"io"
+	"os"
 
 	"github.com/UltimateSoftware/envctl/pkg/container"
 	"github.com/docker/docker/api/types"
 )
 
 // Run runs the given command array on the container with the given metadata.
-func (c *Controller) Run(m container.Metadata, cmd []string) error {
-	err := c.client.ContainerStart(
-		context.Background(),
+func (c *Controller) Run(m container.Metadata, cmd []string) (err error) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	c.mirrorContainerTTY(m.ID)
+
+	err = c.client.ContainerStart(
+		ctx,
 		m.ID,
 		types.ContainerStartOptions{},
 	)
 	if err != nil {
+		cancel()
 		return err
 	}
 
-	resp, err := c.client.ContainerExecCreate(
-		context.Background(),
-		m.ID,
-		types.ExecConfig{
-			AttachStderr: true,
-			AttachStdout: true,
-			Cmd:          cmd,
-		})
+	cfg := types.ExecConfig{
+		AttachStderr: true,
+		AttachStdout: true,
+		Cmd:          cmd,
+		Detach:       false,
+		Tty:          true,
+	}
+
+	resp, err := c.client.ContainerExecCreate(ctx, m.ID, cfg)
 	if err != nil {
+		cancel()
 		return err
 	}
 
-	return c.client.ContainerExecStart(
-		context.Background(),
+	hijacked, err := c.client.ContainerExecAttach(ctx, resp.ID, cfg)
+	if err != nil {
+		cancel()
+		return err
+	}
+
+	errchan := make(chan error)
+	donechan := make(chan struct{})
+	go func(
+		cancel context.CancelFunc,
+		hijacked types.HijackedResponse,
+		stdout *os.File,
+	) {
+		_, err = io.Copy(stdout, hijacked.Reader)
+		if err != nil {
+			cancel()
+			errchan <- err
+		}
+
+		donechan <- struct{}{}
+	}(cancel, hijacked, c.stdout.stream)
+
+	err = c.client.ContainerExecStart(
+		ctx,
 		resp.ID,
 		types.ExecStartCheck{},
 	)
+	if err != nil {
+		cancel()
+		return err
+	}
+
+	select {
+	case err := <-errchan:
+		return err
+	case <-donechan:
+	}
+
+	return nil
 }
